47948de36580e15691065bc8765c769616f6c8ae
sirMSimoya <139569286+msimoya@users.noreply.github.com>
2025-08-26 00:51:12 -0400

    modified the Main

--- IOS101_Prework/ViewController.swift
+++ IOS101_Prework/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var schoolLabel: UILabel!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var displayImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameLabel.isUserInteractionEnabled = true
+        roleLabel.isUserInteractionEnabled = true
+        
+       
+
+        let roleTap = UITapGestureRecognizer(target: self, action: #selector(showComputer))
+        roleLabel.addGestureRecognizer(roleTap)
+    }
+    
+
+    @objc func showComputer() {
+        displayImageView.image = UIImage(named: "roleLabel")
     }
 
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
 
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+
+   
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
 }
-
